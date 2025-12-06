f0d35d0c23275c38d27ead68ba83199a7b268b18
Mohamed Saber <mmms77990@gmail.com>
2025-12-06 14:18:38 +0200

    Add WeatherForecastController with endpoints for weather forecasts and Celsius to Fahrenheit conversion.
    Signed-off-by: Mohamed Saber <mmms77990@gmail.com>

--- Rafeek.API/Controllers/WeatherForecastController.cs
+++ Rafeek.API/Controllers/WeatherForecastController.cs
@@ -30,13 +30,6 @@ namespace Rafeek.API.Controllers
             .ToArray();
         }
 
-        [HttpGet("greet/{name}")]
-        public IActionResult Greet(string name)
-        {
-            var greeting = $"Hello, {name}! Welcome to the Weather Forecast API.";
-            return Ok(greeting);
-        }
-
         [HttpGet("temperature/{celsius}")]
         public IActionResult ConvertToFahrenheit(int celsius)
         {
