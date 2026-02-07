6891edaa2026fd0cf5a9557d30951a207fb5d385
Mohamed Saber <mmms77990@gmail.com>
2026-02-08 00:47:55 +0200

    update connectionstrings (development, live), then add migration to change aspnetusers to applicationusers

---
+++ Rafeek.Persistence/Configurations/IdentityConfiguration/IdentityUserConfigurations.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Rafeek.Persistence.Configurations.IdentityConfiguration
+{
+    public class IdentityUserConfigurations : IEntityTypeConfiguration<IdentityUser<Guid>>
+    {
+        public void Configure(EntityTypeBuilder<IdentityUser<Guid>> builder)
+        {
+            builder.ToTable("ApplicationUsers");
+        }
+    }
+}
