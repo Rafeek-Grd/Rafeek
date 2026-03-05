5f795be3a163ff1597de2f55f29366e1786d9cf4
Mohamed Saber <mmms77990@gmail.com>
2026-03-06 00:47:09 +0200

    update Academic Calendar table

--- Rafeek.Domain/Entities/AcademicCalendar.cs
+++ Rafeek.Domain/Entities/AcademicCalendar.cs
@@ -2,10 +2,14 @@
 
 namespace Rafeek.Domain.Entities
 {
-    public class AcademicCalendar: BaseEntity
+    public class AcademicCalendar : BaseEntity
     {
         public string EventName { get; set; } = string.Empty;
         public string? Description { get; set; }
-        public DateTime EventDate { get; set; } 
+        public DateTime EventDate { get; set; }
+        public TimeSpan StartTime { get; set; }
+        public TimeSpan EndTime { get; set; }
+        public bool IsAllDay { get; set; }
+        public string? Location { get; set; }
     }
 }
---
+++ Rafeek.Persistence/Migrations/Rafeek/20260305224411_UpdateAcademicCalendar.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Rafeek.Persistence.Migrations.Rafeek
+{
+    /// <inheritdoc />
+    public partial class UpdateAcademicCalendar : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<TimeSpan>(
+                name: "EndTime",
+                schema: "dbo",
+                table: "AcademicCalendars",
+                type: "time",
+                nullable: false,
+                defaultValue: new TimeSpan(0, 0, 0, 0, 0));
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsAllDay",
+                schema: "dbo",
+                table: "AcademicCalendars",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Location",
+                schema: "dbo",
+                table: "AcademicCalendars",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<TimeSpan>(
+                name: "StartTime",
+                schema: "dbo",
+                table: "AcademicCalendars",
+                type: "time",
+                nullable: false,
+                defaultValue: new TimeSpan(0, 0, 0, 0, 0));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EndTime",
+                schema: "dbo",
+                table: "AcademicCalendars");
+
+            migrationBuilder.DropColumn(
+                name: "IsAllDay",
+                schema: "dbo",
+                table: "AcademicCalendars");
+
+            migrationBuilder.DropColumn(
+                name: "Location",
+                schema: "dbo",
+                table: "AcademicCalendars");
+
+            migrationBuilder.DropColumn(
+                name: "StartTime",
+                schema: "dbo",
+                table: "AcademicCalendars");
+        }
+    }
+}
