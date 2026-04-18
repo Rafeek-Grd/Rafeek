a620a563283d945d0a47a5a93816fc9b83c9f618
Mohamed Saber <mmms77990@gmail.com>
2026-04-18 03:07:02 +0200

    migrate changes in database

---
+++ Rafeek.Persistence/Migrations/Rafeek/20260418010549_UpdateNullabilityOfDepartmentIdInCourse.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Rafeek.Persistence.Migrations.Rafeek
+{
+    /// <inheritdoc />
+    public partial class UpdateNullabilityOfDepartmentIdInCourse : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "DepartmentId",
+                schema: "dbo",
+                table: "Courses",
+                type: "uniqueidentifier",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "DepartmentId",
+                schema: "dbo",
+                table: "Courses",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldNullable: true);
+        }
+    }
+}
