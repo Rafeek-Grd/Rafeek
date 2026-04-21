64944dbe94f6005c7a2a4fecb0a9edcaee6c9ba4
Mohamed Saber <mmms77990@gmail.com>
2026-04-21 20:36:18 +0200

    savechanges

--- ConfirmEmails.ps1
+++
@@ -1,7 +0,0 @@
-$conn = New-Object System.Data.SqlClient.SqlConnection('Server=db43195.public.databaseasp.net;Database=db43195;User Id=db43195;Password=Mo@123456;Encrypt=False')
-$conn.Open()
-$cmd = $conn.CreateCommand()
-$cmd.CommandText = 'UPDATE [auth].[ApplicationUsers] SET EmailConfirmed = 1'
-$rows = $cmd.ExecuteNonQuery()
-Write-Host "Updated $rows users to have confirmed emails."
-$conn.Close()
